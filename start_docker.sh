d00fc85d1f21d8456f507861bb833ebe293aa68c
Rob M <rob.t.mckennie@gmail.com>
2025-01-13 01:09:26 +1300

    filled out the start script

--- start_docker.sh
+++ start_docker.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-docker run --net host --rm -d -e POSTGRES_PASSWORD=tinykite04 -p 5432:5432 postgres
-docker run --net host --rm -d -v ./web:/var/www/html -p 80:80 -p 443:443 634f7639e84afa09c2b65ae591679637a5d2ee390ea31e19fe929861a7d6ba02
+
+docker built -t h0dgep0dge/pgphpapache .
+
+docker stop postgres httpd
+
+docker run --net host --name postgres --rm -d -e POSTGRES_PASSWORD=tinykite04 -p 5432:5432 postgres
+docker run --net host --name httpd --rm -d -v ./web:/var/www/html -p 80:80 -p 443:443 h0dgep0dge/pgphpapache
+
+sleep 5 # sometimes postgres isn't ready yet lmao
 
 psql -h localhost -U postgres < db.sql
