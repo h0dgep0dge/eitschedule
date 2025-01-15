fb3dd5d195ab8c0fdb4d9cad95c48fa398b9c393
Rob <rob.t.mckennie@gmail.com>
2025-01-15 23:04:22 +1300

    updated schema and start script to work with the data changes

--- schema.sql
+++ schema.sql
@@ -8,21 +8,27 @@ CREATE TABLE Lecturers (
     url VARCHAR(200)
 );
 
+CREATE TYPE CAMPUS_ENUM AS ENUM ('Taradale','Tairawhiti');
+
 CREATE TABLE Courses (
-    name VARCHAR(160) PRIMARY KEY NOT NULL,
+    name VARCHAR(160) NOT NULL,
+    campus CAMPUS_ENUM NOT NULL,
     code VARCHAR(15) NOT NULL DEFAULT 'XXX.XXX',
-    lecturername varchar(80) REFERENCES Lecturers(name) NOT NULL
+    lecturername varchar(80) REFERENCES Lecturers(name),
+    PRIMARY KEY (name, campus)
 );
 
-CREATE TYPE WEEKDAY AS ENUM ('mon','tue','wed','thu','fri','sat','sun');
+CREATE TYPE WEEKDAY_ENUM AS ENUM ('mon','tue','wed','thu','fri','sat','sun');
 
 CREATE TABLE Slots (
     slotID SERIAL PRIMARY KEY NOT NULL,
-    courseName varchar(160) REFERENCES Courses(name) NOT NULL,
-    day WEEKDAY NOT NULL,
+    courseName varchar(160) NOT NULL,
+    campus CAMPUS_ENUM NOT NULL,
+    day WEEKDAY_ENUM NOT NULL,
     slotTime NUMERIC(4,1) NOT NULL,
     slotLength NUMERIC(2,1) NOT NULL,
-    room VARCHAR(15)
+    room VARCHAR(15),
+    FOREIGN KEY (courseName, campus) REFERENCES Courses (name, campus)
 );
 
 CREATE TABLE Sessions (
