8204240d7fd44981fe09817fdd0bbb6c4bf0a67d
Rob M <rob.t.mckennie@gmail.com>
2025-01-20 02:00:18 +1300

    rewrote view.php to use the separate db.php structure

--- web/html/db.php
+++ web/html/db.php
@@ -56,4 +56,49 @@ function closeDB() {
     pg_close($dbconn);
 }
 
+function getSessions($courses,$campus,$week) {
+    global $dbconn;
+
+    $slots = Array();
+
+    $where = "WHERE TRUE";
+
+    $parameters = Array();
+    $i = 1;
+
+    if(!is_null($week)) {
+        $where .= ' AND week = $'.(string)$i++;
+        $parameters[] = (string)$week;
+    }
+
+    if(is_array($courses) && count($courses) > 0){
+        $where .= " AND (FALSE";
+        foreach($courses as $course) {
+            $where .= ' OR Courses.name = $'.(string)$i++;
+            $parameters[] = $course;
+        }
+        $where .= ")";
+    }
+
+    if(is_string($campus) && $campus != '') {
+        $where .= ' AND Courses.campus = $'.(string)$i++;
+        $parameters[] = $campus;
+    }
+
+    $query = "SELECT Courses.name,Courses.lecturername,Lecturers.url,Slots.room,Slots.day,Sessions.week,Slots.slottime,Slots.slotlength FROM Sessions ".
+             "JOIN Slots ON Sessions.slotID = Slots.slotID ".
+             "JOIN Courses ON Slots.courseName = Courses.name AND Slots.campus = Courses.campus ".
+             "LEFT JOIN Lecturers ON Courses.lecturername = Lecturers.name ".
+             $where.
+             " ORDER BY Slots.slottime;";
+    
+    $result = pg_query_params($dbconn, $query, $parameters) or die('Query failed: ' . pg_last_error());
+
+    while($row = pg_fetch_array($result,null,PGSQL_ASSOC))
+        $slots[] = $row;
+
+    pg_free_result($result);
+
+    return $slots;
+}
 ?>
--- web/html/view.php
+++ web/html/view.php
@@ -4,71 +4,9 @@
 </head>
 <body>
 <?php
-function get_schedule($conn,$day,$week,$courses,$campus) {
-
-    $where = "WHERE Sessions.week = ".(string)$week." AND Slots.day = '".$day."' ";
-    if(is_array($courses) && count($courses) > 0){
-        $where .= "AND (FALSE";
-        foreach($courses as $course)
-            $where .= " OR Courses.name = '".$course."'";
-        $where .= ") ";
-    }
-
-    if(is_string($campus) && $campus != '') {
-        $where .= " AND Courses.campus ='".$campus."' ";
-    }
-
-    $query = "SELECT Courses.name,Courses.lecturername,Lecturers.url,Slots.room,Slots.slottime,Slots.slotlength FROM Sessions ".
-             "JOIN Slots ON Sessions.slotID = Slots.slotID ".
-             "JOIN Courses ON Slots.courseName = Courses.name AND Slots.campus = Courses.campus ".
-             "LEFT JOIN Lecturers ON Courses.lecturername = Lecturers.name ".
-             $where.
-             "ORDER BY Slots.slottime;";
-    //echo $query,PHP_EOL;
-    $result = pg_query($conn, $query) or die('Query failed: ' . pg_last_error());
-    
-    $slots = Array();
-
-    $schedule = Array();
-
-    while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
-        $slots[] = Array($line["name"],$line["room"],(int)($line["slottime"]),(int)($line["slotlength"]),$line["lecturername"],$line["url"]);
-    }
-
-    while(count($slots) > 0) {
-        $line = Array();
-        $next = Array();
-        $line[] = $slots[0];
-        array_shift($slots);
-        while(count($slots) > 0) {
-            if($slots[0][2] >= $line[count($line)-1][2] + $line[count($line)-1][3]) {
-                $line[] = $slots[0];
-            } else {
-                $next[] = $slots[0];
-            }
-            array_shift($slots);
-        }
-        $schedule[] = $line;
-        $slots = $next;
-    }
-
-    pg_free_result($result);
-
-    return $schedule;
-}
+require_once('./db.php');
 
 $days = Array('mon'=>'Monday','tue'=>'Tuesday','wed'=>'Wednesday','thu'=>'Thursday','fri'=>'Friday','sat'=>'Saturday','sun'=>'Sunday');
-
-// Connecting, selecting database
-$dbconn = pg_connect("host=db dbname=schedule user=postgres")
-    or die('Could not connect: ' . pg_last_error());
-
-echo "<table class='outer'>\n";
-echo "<tr class='outer'><th class='outer'></th>";
-for($i = 7;$i < 17;$i += 0.5) {
-    echo "<th class='outer'>",$i,"</th>";
-}
-echo "</tr>";
 
 if(isset($_GET['courses']))
     $courses = $_GET['courses'];
@@ -85,45 +23,84 @@ if(isset($_GET['campus']))
 else
     $campus = '';
 
-foreach(Array('mon','tue','wed','thu','fri') as $day) {
-#foreach(Array('mon') as $day) {
-    $schedule = get_schedule($dbconn,$day,$week,$courses,$campus);
+$sessions = getSessions($courses,$campus,$week);
 
+$sessionsPerDay = Array('mon'=>Array(),'tue'=>Array(),'wed'=>Array(),'thu'=>Array(),'fri'=>Array(),'sat'=>Array(),'sun'=>Array());
 
-
-$tr = "<tr class='outer'><td class='outer' rowspan=".(string)count($schedule).">".$days[$day]."</td>";
-foreach($schedule as $line) {
-    echo $tr;
-    $tr = "<tr>";
-    for($i = 7;$i < 17;) {
-        if(count($line) == 0 || $line[0][2] > $i) {
-            echo "<td class='outer'></td>";
-            $i += 0.5;
-            continue;
-        }
-        if($line[0][2] == $i) {
-            echo "<td class='outer' colspan='",$line[0][3]*2,"'>";
-            echo "<table class='inner'>";
-            echo "<tr class='inner'><td colspan='3' class='inner'>",$line[0][0],"</td></tr>";
-            if($line[0][4] == '') // No lecturer for this class yet!
-                echo "<tr class='inner'><td class='left' colspan='2'>",$line[0][1],"</td><td class='right'>TBD</td></tr>";
-            else
-                echo "<tr class='inner'><td class='left' colspan='2'>",$line[0][1],"</td><td class='right'><a href='".$line[0][5]."'>",$line[0][4],"</a></td></tr>";
-            echo "</table>";
-            $i += $line[0][3];
-            array_shift($line);
-        }
-    }
-    echo "</tr>";
+foreach($sessions as $session) {
+    $sessionsPerDay[$session['day']][] = $session;
 }
+
+$sortedSessionsPerDay = Array();
+
+foreach($sessionsPerDay as $day => $sessions) {
+    if(count($sessions) == 0) continue;
+    $unsorted = $sessions;
+    $sorted = Array();
+
+    while(count($unsorted) > 0) {
+        $line = Array();
+        $next = Array();
+
+        $line[] = $unsorted[0];
+        array_shift($unsorted);
+
+        while(count($unsorted) > 0) {
+            if($unsorted[0]['slottime'] >= $line[count($line)-1]['slottime'] + $line[count($line)-1]['slotlength']) {
+                $line[] = $unsorted[0];
+            } else {
+                $next[] = $unsorted[0];
+            }
+            array_shift($unsorted);
+        }
+        $sorted[] = $line;
+        $unsorted = $next;
+    }
+
+    $sortedSessionsPerDay[$day] = $sorted;
+}
+
+echo "<table class='outer'>\n";
+echo "<tr class='outer'><th class='outer'></th>";
+for($i = 7;$i < 17;$i += 0.5) {
+    echo "<th class='outer'>",$i,"</th>";
+}
+echo "</tr>";
+
+foreach($sortedSessionsPerDay as $day => $schedule) {
+
+    $tr = "<tr class='outer'><td class='outer' rowspan=".(string)count($schedule).">".$days[$day]."</td>";
+    foreach($schedule as $line) {
+        echo $tr;
+        $tr = "<tr>";
+        for($i = 7;$i < 17;) {
+            if(count($line) == 0 || $line[0]['slottime'] > $i) {
+                echo "<td class='outer'></td>";
+                $i += 0.5;
+                continue;
+            }
+            if($line[0]['slottime'] == $i) {
+                $session = $line[0];
+                echo "<td class='outer' colspan='",$session['slotlength']*2,"'>";
+                echo "<table class='inner'>";
+                echo "<tr class='inner'><td colspan='3' class='inner'>",$session['name'],"</td></tr>";
+                if($session['lecturername'] == '') // No lecturer for this class yet!
+                    echo "<tr class='inner'><td class='left' colspan='2'>",$session['room'],"</td><td class='right'>TBD</td></tr>";
+                else
+                    echo "<tr class='inner'><td class='left' colspan='2'>",$session['room'],"</td><td class='right'><a href='".$session['room']."'>",$session['lecturername'],"</a></td></tr>";
+                echo "</table>";
+                $i += $session['slotlength'];
+                array_shift($line);
+            }
+        }
+        echo "</tr>";
+    }
 
 }
 
 echo "</table>\n";
 
-
-// Closing connection
-pg_close($dbconn);
+closeDB();
 echo "Click <a href='export.php?".$_SERVER['QUERY_STRING']."'>here</a> to export these courses as an iCal file. Please take care! I will not take responsibility for how annoying these events are to delete if they're worng.";
 ?>
 </body>
